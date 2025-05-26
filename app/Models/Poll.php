cf19551d1e78f60852215294b0935c087335fc07
fzengin19 <fatihzengin654@outlook.com>
2025-05-27 01:50:48 +0300

    Created Poll model and migration

---
+++ app/Models/Poll.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Poll extends Model
+{
+    protected $fillable = [
+        'title',
+        'voter_identifier'
+    ];
+}
