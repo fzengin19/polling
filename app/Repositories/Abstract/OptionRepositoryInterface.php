136e5a0516ef443530e77590e17cbdc7e07301ae
fzengin19 <fatihzengin654@outlook.com>
2025-05-31 02:05:18 +0300

    Added Option and PollVote repos

---
+++ app/Repositories/Abstract/OptionRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Repositories\Abstract;
+
+use App\Core\BaseRepositoryInterface;
+use App\Models\Option;
+
+interface OptionRepositoryInterface extends BaseRepositoryInterface
+{
+    
+}
---
+++ app/Repositories/Eloquent/PollVoteRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Abstract\PollVoteRepositoryInterface;
+use App\Core\BaseRepository;
+use App\Models\PollVote;
+
+class PollVoteRepository extends BaseRepository implements PollVoteRepositoryInterface
+{
+    public function __construct(PollVote $model)
+    {
+        $this->model = $model;
+    }
+
+
+}
